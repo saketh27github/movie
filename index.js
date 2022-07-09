b0529620c1335e5dc2f560a951cddbf8e4b23a4d
Saketh Reddy <Sakethreddy1998@gmail.com>
2022-07-09 19:58:26 +0530

    added new commit

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function loadMovieDetails(){
             // console.log(movie.dataset.id);
             searchList.classList.add('hide-search-list');
             movieSearchBox.value = "";
-            const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=52d6585f`);
+            const result = await fetch(`https://www.omdbapi.com/?i=${movie.dataset.id}&apikey=52d6585f`);
             const movieDetails = await result.json();
 
             // console.log(movieDetails);
